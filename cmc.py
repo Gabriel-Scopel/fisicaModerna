ec84cd5838d399ac4ffce4c28346daf4067e1fc6
Gabriel de Souza Scopel <93132666+Gabriel-Scopel@users.noreply.github.com>
2022-08-27 21:39:16 -0300

    Implementando f2 e corrigindo bugs

--- cmc.py
+++ cmc.py
@@ -1,7 +1,7 @@
 """ Gabriel Souza Scopel - 222210262    
 Kaique da Silva Fernandes - 222210114
 Murilo Carvalho Povoa da Silva - 222210346
-Matheus Arcanjo """
+Matheus T da Silva Arcanjo - 22221020-5 """
 
 import math
 pi = math.pi
@@ -22,124 +22,82 @@ def f1():
   a=input("Digite o código correspondente a fórmula que você deseja utilizar:")
   if(a == "1"):
     lambida = float(input("Digite o valor de Lambida: "))
-    print((2*pi)/lambida)
+    print((2*pi)/lambida, "rad/m")
   elif(a == "2"):
     w = float(input("Digite o valor de w: "))
-    print(v/w)
+    print(v/w, "rad/")
   elif(a == "3"):
     T = float(input("Digite o valor de T: "))
-    print((2*pi)/T)
+    print((2*pi)/T, "rad/s")
   elif(a == "4"):
     f = float(input("Digite o valor de f: "))
-    print((2*pi)/f)
+    print((2*pi)*f, "rad/s")
   elif(a == "5"):
     k = float(input("Digite o valor de k: "))
-    print((2*pi)/k)
+    print((2*pi)/k, "m")
   elif(a == "6"):
     f = float(input("Digite o valor de f: "))
-    print(v/f)
+    print(v/f, "m")
   elif(a == "7"):
     w = float(input("Digite o valor de w: "))
-    print(w/2*pi)
+    print(w/2*pi, "Hz")
   elif(a == "8"):
     lambida = float(input("Digite o valor de lambida: "))
-    print(v/lambida)
+    print(v/lambida, "Hz")
   else:
     print("Digite um comando válido.")
     
     
+def f2():
+  
+  print("1 - Em = c*Bm")
+  print("2 - Bm = Em/c")
+  print("3 - I = E^2/2mi*c")
+  print("4 - I = c*B^2/2mi")
+  a=input("Digite o código correspondente a fórmula que você deseja utilizar:")
+  if(a=="1"):
+    Bm = float(input("Digite o valor de Bm: "))
+    print(c*Bm, "V/m")
+  elif(a=="2"):
+    Em = float(input("Digite o valor de Em: "))
+    print(Em/c, "T")
+  elif(a=="3"):
+    E = float(input("Digite o valor de E: "))
+    print((E*E)/2*mi*c, "W/m^2")
+  elif(a=="4"):
+    B = float(input("Digite o valor de B: "))
+    print((c*B*B)/2*mi, "W/m^2")
+  else:
+    print("Digite um comando válido")
     
-def Em():
-    Em = float(input("Digite o valor de Em:"))
-    Bm = Em/c
-    I = Em**2/(2*mi*c)
-    print(f"Bm: {Bm:.2e}", Bm)
-    print(f"I: {I:.2e}",I)
-
-def Bm():
-    Bm = float(input("Digite o valor de Bm:"))
-    Em = c*Bm
-    I = Bm**2/2*mi
-    print(f"Em: {Em:.2e}", Em)
-    print(f"I: {I:.2e}",I)
-    
-def I():
-    I = float(input("Digite o valor de I:"))
-    Em = sqrt(I*2*mi*c)
-    Bm = sqrt(I*2*mi)
-    print(f"Bm: {Bm:.2e}",Bm)
-    print(f"Em: {Em:.2e}", Em)
-
-def f():
-    f = float(input("Digite o valor de f:"))
-    w = 2*pi*f
-    lambida = c/f
-    k = f/(c*2*pi)
-    print(f"w: {w:.2e}", w)
-    print(f"lambida: {lambida:.2e}", lambida)
-    print(f"k: {k:.2e}",k)
-    
-def lambida():
-    lambida = float(input("Digite o valor de lambida:"))
-    k = (2*pi)/lambida
-    f = c/lambida
-    w = c*2*pi/lambida
-    print(f"w: {w:.2e}",w)
-    print(f"f: {f:.2e}", f)
-    print(f"k: {k:.2e}", k)
-    
-def k():
-    k = float(input("Digite o valor de k: "))
-    lambida = (2*pi)/k
-    f = k*c*2*pi
-    w = k*c*4*pi
-    print(f"w: {w:.2e}", w)
-    print(f"lambida: {lambida:.2e}", lambida)
-    print(f"f: {f:.2e}", f)
-    
-def w():
-    w = float(input("Digite o valor de w:"))
-    f = w/(2*pi)
-    lambida = c*2*pi/w
-    k = w/(c*4*pi)
-    print(f"f:{f:.2e}", f)
-    print(f"lambida: {lambida:.2e}",lambida)
-    print(f"k: {k:.2e}",k)
-
-
 
 
 def main():
-    
-   # print("Você deseja calcular f, lambida, k ou w?")
-    print("Digite S para sim e N para não")
-    a = input("Você deseja calcular f, lambida, k ou w?")
+    print("Este programa tem por finalidade realizar cálculos envolvendo Ondas eletromagnéticas. Nele é possível calcular frequência, lambida, número de ondas e frequência angular, Campo Magnético, Intensidade e Campo elétrico \n")
+    print("Todas as unidades a serem digitadas devem estas no sistema internacional \n")
+  
+    print("Integrantes do grupo:")
+    print("Gabriel Souza Scopel")
+    print("Kaique da Silva Fernandes")
+    print("Murilo Carvalho Povoa da Silva")
+    print("Matheus Arcanjo")
+    print()
+    input("Digite Enter para continuar...")
+    print("Você deseja calcular f, lambida, k ou w?")
+    a = input("Digite S para sim e N para não: ")
+     
     if(a=="S"):
         f1()
     elif(a=="N"):
-        print("a")
+      print("Você deseja calcular Bm, Em ou I?")
+      b = input("Digite S para sim e N para não:")
+      if(b == "S"):
+        f2()
+      else:
+        print("Reinicie o programa.")
+      
         
     else:
         print("Reinicie e digite um comando válido.")
-    
-    
-    """ count = input('qual formula? ')
-    
-    if count == '1':
-        Em()
-    elif count == '2':
-        Bm() 
-    elif count == '3':
-        I()
-    elif count == '4':
-        f()
-    elif count == '5':
-        lambida()
-    elif count == '6':
-        k()
-    elif count == '7':
-        w()
-    else:
-        print("Você digitou um comando inválido.") """
 
 main()
